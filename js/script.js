abc9fb7c9358a9cdf055ff014398b85b3337dc07
Anna S <anna.sundberg@geshdo.com>
2024-04-30 09:48:15 +0200

    change title font and some styling

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const computerPointsh3 = document.querySelector("#computer-points");
 const resultText = document.querySelector("#result-text");
 let playerPoints = 0;
 let computerPoints = 0;
+let winnerDeclared = false;
 
 const array = ["rock", "paper", "scissors"];
 
@@ -15,69 +16,73 @@ document.querySelector("#player-wins").style.display = "none";
 document.querySelector("#computer-wins").style.display = "none";
 
 startBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    document.querySelector("#game-container").style.display = "block";
-    document.querySelector("#go").style.display = "block";
-    document.querySelector("#go").innerText =
-        "Let's go, " + gamerName.value + "!";
+  event.preventDefault();
+  document.querySelector("#game-container").style.display = "block";
+  document.querySelector("#go").style.display = "block";
+  document.querySelector("#go").innerText =
+    "Let's go, " + gamerName.value + "!";
 
-    let playerText = document.querySelector("#player-text");
-    let computerText = document.querySelector("#computer-text");
+  let playerText = document.querySelector("#player-text");
+  let computerText = document.querySelector("#computer-text");
 
-    gameContainer.addEventListener("click", function (event) {
-        let randomNumber = Math.floor(Math.random() * array.length);
-        const userChoice = event.target.id;
-        const computerChoice = array[randomNumber];
+  gameContainer.addEventListener("click", function (event) {
+    if (winnerDeclared) return; // Do nothing if winner has been declared
 
-        if (event.target.tagName === "BUTTON") {
-            playerText.innerText = gamerName.value + ": " + userChoice;
-            computerText.innerText = "Computer: " + computerChoice;
+    let randomNumber = Math.floor(Math.random() * array.length);
+    const userChoice = event.target.id;
+    const computerChoice = array[randomNumber];
 
-            if (
-                (userChoice == "rock" && computerChoice == "scissors") ||
-                (userChoice == "scissors" && computerChoice == "paper") ||
-                (userChoice == "paper" && computerChoice == "rock")
-            ) {
-                playerPoints++;
-                resultText.innerText = "Du fick poäng!";
-            } else if (
-                (computerChoice == "paper" && userChoice == "rock") ||
-                (computerChoice == "rock" && userChoice == "scissors") ||
-                (computerChoice == "scissors" && userChoice == "paper")
-            ) {
-                computerPoints++;
-                resultText.innerText = "Datorn fick poäng!";
-            } else if (userChoice == computerChoice) {
-                resultText.innerText = "Ingen fick poäng!";
-            }
-        }
+    if (event.target.tagName === "BUTTON") {
+      playerText.innerText = gamerName.value + ": " + userChoice;
+      computerText.innerText = "Computer: " + computerChoice;
 
-        playerPointsh3.innerText = "Dina poäng:" + playerPoints + "/3";
-        computerPointsh3.innerText = "Datorns poäng:" + computerPoints + "/3";
+      if (
+        (userChoice == "rock" && computerChoice == "scissors") ||
+        (userChoice == "scissors" && computerChoice == "paper") ||
+        (userChoice == "paper" && computerChoice == "rock")
+      ) {
+        playerPoints++;
+        resultText.innerText = "Du fick poäng!";
+      } else if (
+        (computerChoice == "paper" && userChoice == "rock") ||
+        (computerChoice == "rock" && userChoice == "scissors") ||
+        (computerChoice == "scissors" && userChoice == "paper")
+      ) {
+        computerPoints++;
+        resultText.innerText = "Datorn fick poäng!";
+      } else if (userChoice == computerChoice) {
+        resultText.innerText = "Ingen fick poäng!";
+      }
+    }
 
-        if (computerPoints == 3) {
-            document.querySelector("#computer-wins").style.display = "block";
-            winner();
-        } else if (playerPoints == 3) {
-            document.querySelector("#player-wins").style.display = "block";
-            winner();
-        }
-    });
+    playerPointsh3.innerText = "Dina poäng:" + playerPoints + "/3";
+    computerPointsh3.innerText = "Datorns poäng:" + computerPoints + "/3";
+
+    if (computerPoints == 3) {
+      document.querySelector("#computer-wins").style.display = "block";
+      winner();
+    } else if (playerPoints == 3) {
+      document.querySelector("#player-wins").style.display = "block";
+      winner();
+    }
+  });
 });
 
 function winner() {
-    computerPoints = 0;
-    playerPoints = 0;
+  winnerDeclared = true;
 
-    const btns = document.querySelectorAll(".game-btns");
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].style.display = "none";
-    }
+  computerPoints = 0;
+  playerPoints = 0;
 
-    const restartBtn = document.createElement("button");
-    gameContainer.appendChild(restartBtn);
-    restartBtn.innerText = "PLAY AGAIN";
-    restartBtn.addEventListener("click", function () {
-        location.reload();
-    });
+  const btns = document.querySelectorAll(".game-btns");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].style.display = "none";
+  }
+
+  const restartBtn = document.createElement("button");
+  gameContainer.appendChild(restartBtn);
+  restartBtn.innerText = "PLAY AGAIN";
+  restartBtn.addEventListener("click", function () {
+    location.reload();
+  });
 }
